71c0161eb60d05e2209281d10c1ec1ff19a3281e
abdoboeriy <eng.abdoboeriy@gmail.com>
2025-10-09 13:38:47 +0300

    add context Type

--- src/Context/ContextProvider.tsx
+++ src/Context/ContextProvider.tsx
@@ -1,41 +1,98 @@
-"use client";
-import React, { createContext, useEffect, useState } from "react";
+// "use client";
+// import React, { createContext, useEffect, useState } from "react";
 
-interface children {
+// interface children {
+//   children: React.ReactNode;
+// }
+
+// interface StoreContextType {
+//   setOpenMenu: React.Dispatch<React.SetStateAction<boolean>>;
+//   openMenu: boolean;
+//   sortBy: string;
+//   setSortBy: React.Dispatch<React.SetStateAction<string>>;
+// }
+
+// export const StoreContext = createContext<StoreContextType>({
+//   setOpenMenu: () => {},
+//   openMenu: false,
+//   sortBy: "",
+//   setSortBy: () => {},
+// });
+
+// const ContextProvider = ({ children }: children) => {
+//   const [openMenu, setOpenMenu] = useState(false);
+//   const [sortBy, setSortBy] = useState<string>("all");
+
+//   return (
+//     <>
+//       <StoreContext.Provider
+//         value={{
+//           openMenu,
+//           setOpenMenu,
+//           sortBy,
+//           setSortBy,
+//         }}
+//       >
+//         {children}
+//       </StoreContext.Provider>
+//     </>
+//   );
+// };
+
+// export default ContextProvider;
+
+"use client";
+import React, { createContext, useState, useEffect } from "react";
+import { ArrivalsProduct } from "@/Types/arrivalsProductsType";
+
+interface ChildrenProps {
   children: React.ReactNode;
 }
 
 interface StoreContextType {
-  setOpenMenu: React.Dispatch<React.SetStateAction<boolean>>;
   openMenu: boolean;
+  setOpenMenu: React.Dispatch<React.SetStateAction<boolean>>;
   sortBy: string;
   setSortBy: React.Dispatch<React.SetStateAction<string>>;
+  arrivalsProducts: ArrivalsProduct[];
+  setArrivalsProducts: React.Dispatch<React.SetStateAction<ArrivalsProduct[]>>;
 }
 
 export const StoreContext = createContext<StoreContextType>({
-  setOpenMenu: () => {},
   openMenu: false,
-  sortBy: "",
+  setOpenMenu: () => {},
+  sortBy: "all",
   setSortBy: () => {},
+  arrivalsProducts: [],
+  setArrivalsProducts: () => {},
 });
 
-const ContextProvider = ({ children }: children) => {
+const ContextProvider = ({ children }: ChildrenProps) => {
   const [openMenu, setOpenMenu] = useState(false);
-  const [sortBy, setSortBy] = useState<string>("all");
+  const [sortBy, setSortBy] = useState("all");
+  const [arrivalsProducts, setArrivalsProducts] = useState<ArrivalsProduct[]>(
+    []
+  );
+
+  // مثال بسيط لتجربة الـ context
+  // useEffect(() => {
+  //   // هنا ممكن بعدين تجيب الداتا من API
+  //   // setArrivalsProducts(data);
+  // }, []);
 
   return (
-    <>
-      <StoreContext.Provider
-        value={{
-          openMenu,
-          setOpenMenu,
-          sortBy,
-          setSortBy,
-        }}
-      >
-        {children}
-      </StoreContext.Provider>
-    </>
+    <StoreContext.Provider
+      value={{
+        openMenu,
+        setOpenMenu,
+        sortBy,
+        setSortBy,
+        arrivalsProducts,
+        setArrivalsProducts,
+      }}
+    >
+      {children}
+    </StoreContext.Provider>
   );
 };
 
